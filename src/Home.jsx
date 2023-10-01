f9ed204760feb56c794109bc5c27ff0b0c3a859b
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-10-02 02:17:15 +0530

    New

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
           <div className='row'>
             <div className='col-lg-6'>
               <div>
-                <h1 className='primary'>DECENTRALIZED SELF-CUSTODIAL LEVERAGED TRADING</h1>
-                <h1>Supercharged for Traders</h1>
+                <h1 className='hero-highlight'>DECENTRALIZED SELF-CUSTODIAL LEVERAGED TRADING</h1>
+                <h1 className='hero-heading'>Supercharged for Traders</h1>
                 <p>Trade crypto with zero price impact, up to 100x leverage and aggregated liquidity. MUX protocol takes care of all the hassles so that you can experience optimized DEX trading on our platform.</p>
-                <button>Start Trading</button>
+                <button className='btn hero-btn'>Start Trading</button>
               </div>
             </div>
           </div>
@@ -22,16 +22,20 @@ const Home = () => {
       </section>
       <section>
         <div className='container'>
-          <h1 className='text-center'>Level Up Every Trade</h1>
+          <h1 className='text-center section-heading'>Level Up Every Trade</h1>
           <div className='row'>
             {
               tokenomics.map((tokenomic) => {
                 const { id, heading, description, image } = tokenomic;
                 return (
                   <div className='col-lg-4' key={id}>
-                    <img src={require(`./Img/home/${image}`)} alt="" />
-                    <h1 className=''>{heading}</h1>
-                    <p>{description}</p>
+                    <div className='tokenomic-item'>
+                      <div className='tokenomic-image'>
+                        <img src={require(`./Img/home/${image}`)} alt="" />
+                      </div>
+                      <h1 className=''>{heading}</h1>
+                      <p>{description}</p>
+                    </div>
                   </div>
                 )
               })
@@ -41,19 +45,21 @@ const Home = () => {
       </section>
       <section>
         <div className='container'>
-          <h1 className='text-center'>All-In-One Leveraged Trading Platform</h1>
+          <h1 className='text-center section-heading'>All-In-One Leveraged Trading Platform</h1>
           <div>
             {
               about.map((item) => {
                 const { id, heading, description, image } = item;
                 return (
-                  <div className="row" key={id}>
-                    <div className={`col-lg-6 ${heading === 'Smart Position Routing' ? 'order-md-2' : ''}`}>
-                      <h1>{heading}</h1>
-                      <p>{description}</p>
+                  <div className="row mt-5" key={id}>
+                    <div className={`col-lg-6 d-flex align-items-center ${heading === 'Smart Position Routing' ? 'order-md-2' : ''}`}>
+                      <div>
+                        <h1 class="about-heading">{heading}</h1>
+                        <p className='about-para'>{description}</p>
+                      </div>
                     </div>
-                    <div className='col-lg-6'>
-                      <img src={require(`./Img/home/About/${image}`)} alt="" />
+                    <div className='col-lg-6 d-flex align-items-center'>
+                      <img src={require(`./Img/home/About/${image}`)} className='img-fluid' alt="" />
                     </div>
                   </div>
                 )
@@ -66,21 +72,23 @@ const Home = () => {
         <div className='features'>
           <div className='container'>
             <div className='row'>
-              <div className='col-lg-4'>
-                <img src={require('./Img/home/feature/feature-main.png')} className='img-fluid' alt="" />
+              <div className='col-lg-4' style = {{"position": "relative"}}>
+                <img src={require('./Img/home/feature/feature-main.png')} className='feature-image' alt="" />
               </div>
               <div className='col-lg-8 order-md-2'>
                 <div className='features-wrapper'>
-                  <h1>New Technology Behind Every Feature</h1>
+                  <h1 className='section-heading text-left mt-5'>New Technology Behind Every Feature</h1>
                   <div className='row'>
                     {
                       features.map((feature) => {
                         const { id, heading, description, image } = feature;
                         return (
                           <div className='col-lg-6' key={id}>
-                            <img src={require(`./Img/home/feature/${image}`)} alt="" />
-                            <h1>{heading}</h1>
-                            <p>{description}</p>
+                            <div className='feature-card'>
+                              <img src={require(`./Img/home/feature/${image}`)} className = 'img-fluid mb-3' alt="" />
+                              <h1 className='feature-heading mb-3'>{heading}</h1>
+                              <p className='feature-para'>{description}</p>
+                            </div>
                           </div>
                         )
                       })
@@ -121,54 +129,54 @@ const Home = () => {
       </section>
       <section>
         <div className='container'>
-          <h1 className='text-center'>Backers</h1>
+          <h1 className='text-center section-heading'>Backers</h1>
           <div className='row mt-5'>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
-            <div className='col-lg-3'>
+            <div className='col-lg-3 text-center'>
               <img src={require('./Img/home/backers/1.png')} alt="" />
             </div>
           </div>
--- src/components/Home/features.js
+++ src/components/Home/features.js
@@ -1,27 +1,27 @@
 const features = [
     {
         "id": 1,
-        "heading": "Zero Price Impact",
-        "description": "$100 or $1,000,000 position, the price impact will always be 0%",
+        "heading": "Universal Liquidity",
+        "description": "The MUX liquidity pool unifies assets from all deployed chains, supported by multiplexing layer infrastructures.",
         "image": "1.svg"
     },
     {
         "id": 2,
-        "heading": "Zero Price Impact",
-        "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "1.svg"
+        "heading": "Liquidity Multiplexing",
+        "description": "Pooled liquidity will be dynamically shared between MUX margin trading and third-party DEX mining across chains to achieve high utilization.",
+        "image": "2.svg"
     },
     {
         "id": 3,
-        "heading": "Zero Price Impact",
-        "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "1.svg"
+        "heading": "Liquidity Aggregation",
+        "description": "MUX aggregates scattered liquidity from different protocols, further offering a consistent and convenient trading experience.",
+        "image": "3.svg"
     },
     {
         "id": 4,
-        "heading": "Zero Price Impact",
-        "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "1.svg"
+        "heading": "Multi-Asset Pool",
+        "description": "MUX liquidity pool holds a portfolio of blue-chip assets and uses dynamic open interest caps to eliminate traders' counterparty risks.",
+        "image": "4.svg"
     }
 ]
 
