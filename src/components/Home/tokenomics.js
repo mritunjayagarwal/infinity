cfe0e14ad63b952f182e7cdcd5d47b599ab63c3b
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2023-10-02 14:12:30 +0530

    nn

--- src/components/Home/tokenomics.js
+++ src/components/Home/tokenomics.js
@@ -9,31 +9,31 @@ const tokenomics = [
         "id": 2,
         "heading": "Zero Price Impact",
         "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "toke-1.svg"
+        "image": "toke-2.svg"
     },
     {
         "id": 3,
         "heading": "Zero Price Impact",
         "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "toke-1.svg"
+        "image": "toke-3.svg"
     },
     {
         "id": 4,
         "heading": "Zero Price Impact",
         "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "toke-1.svg"
+        "image": "toke-4.svg"
     },
     {
         "id": 5,
         "heading": "Zero Price Impact",
         "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "toke-1.svg"
+        "image": "toke-5.svg"
     },
     {
         "id": 6,
         "heading": "Zero Price Impact",
         "description": "$100 or $1,000,000 position, the price impact will always be 0%",
-        "image": "toke-1.svg"
+        "image": "toke-6.svg"
     }
 ]
 
